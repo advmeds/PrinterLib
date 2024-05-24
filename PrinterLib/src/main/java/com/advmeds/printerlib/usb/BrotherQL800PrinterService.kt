7635964df12c1a2a5e695237eb14e9001d970706
Kevin <kevin@advmeds.com>
2024-05-24 18:06:30 +0800

    QL800支援嘉義西區個資貼紙列印

--- PrinterLib/src/main/java/com/advmeds/printerlib/usb/BrotherQL800PrinterService.kt
+++ PrinterLib/src/main/java/com/advmeds/printerlib/usb/BrotherQL800PrinterService.kt
@@ -1,12 +1,17 @@
 package com.advmeds.printerlib.usb
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.graphics.pdf.PdfDocument
 import android.graphics.pdf.PdfDocument.PageInfo
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
-import android.util.Log
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.UnderlineSpan
 import android.util.TypedValue
 import android.widget.TextView
 import com.brother.ptouch.sdk.connection.UsbConnection
@@ -17,10 +22,10 @@ import com.brother.sdk.lmprinter.PrinterDriver
 import com.brother.sdk.lmprinter.PrinterDriverGenerator
 import com.brother.sdk.lmprinter.PrinterModel
 import com.brother.sdk.lmprinter.setting.QLPrintSettings
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.oned.Code128Writer
 import java.io.File
-import java.io.FileNotFoundException
 import java.io.FileOutputStream
-import java.io.IOException
 
 class BrotherQL800PrinterService(private val context: Context) : UsbPrinterService(context.getSystemService(Context.USB_SERVICE) as UsbManager) {
     private var driver: PrinterDriver? = null
@@ -64,105 +69,179 @@ class BrotherQL800PrinterService(private val context: Context) : UsbPrinterServi
 
     fun printPdf() {
         val driver = requireNotNull(driver) { "The printer is not opened." }
-        val pdfFile = buildPdf("test.pdf", "Title", "context")
+        val pdfFile = buildChiaYiInfo(
+            "  陳大同  ",
+            "  A1234846892  ",
+            "  0974593672  ",
+            "  80/07/03  ",
+            "  嘉義市東區復原莊191號12樓  "
+        )
         val settings = QLPrintSettings(PrinterModel.QL_800).apply {
             workPath = context.getExternalFilesDir(null)?.absolutePath ?: ""
             isAutoCut = true
-            labelSize = QLPrintSettings.LabelSize.RollW62RB
+            labelSize = QLPrintSettings.LabelSize.DieCutW29H90
         }
         val error = driver.printPDF(pdfFile, settings)
         require(error.code == ErrorCode.NoError) {
             error.code.toString()
         }
-//        val printerInfo = PrinterInfo().apply {
-//            port = PrinterInfo.Port.USB
-//            printerModel = PrinterInfo.Model.QL_800
-//        }
-//        val printer = Printer().apply {
-//            this.printerInfo = printerInfo
-//        }
-//        val pdfFile = buildPdf("test.pdf", "Title", "context")
-//        printer.startCommunication()
-//        val result = printer.printPdfFile(pdfFile, 1)
-//        printer.endCommunication()
     }
 
-    private fun buildPdf(filename: String, headingText: String, bodyText: String): String? {
+    /** 建構嘉義西區的個資貼紙 */
+    private fun buildChiaYiInfo(
+        name: String,
+        idNo: String,
+        mobile: String,
+        birth: String,
+        address: String
+    ): String? {
         // create a new document
         val document = PdfDocument()
 
-        //TODO: Calculate the height based on number of lines fed in
-        val headerTextSize = 30
-        val bodyTextSize = 20
-
-        //Calulate total pixels height by finding newlines
-        var lastIndex = 0
-        var count = 0
-        while (lastIndex != -1) {
-            lastIndex = bodyText.indexOf("\n", lastIndex)
-            if (lastIndex != -1) {
-                count++
-                lastIndex += "\n".length
-            }
-        }
-        val pageWidth = 234
-        val pageHeight = headerTextSize * 2 + count * bodyTextSize + bodyTextSize
-
+        val textSize = 64
+        val pageWidth = 580
+        val pageHeight = 1800
+        val top = -(pageHeight - pageWidth).toFloat()
         // crate a page description
         val pageInfo = PageInfo.Builder(pageWidth, pageHeight, 1).create()
 
         // start a page
-        val page = document.startPage(pageInfo)
+        val page = document.startPage(pageInfo).apply {
+            canvas.rotate(-90f)
+            canvas.translate(-pageWidth.toFloat(), 0f)
+        }
 
-        // draw something on the page
+        // Adding title
+        TextView(context).apply {
+            val titleSpannedString = SpannableStringBuilder().apply {
+                val text = String.format("姓名 %s 身分證字號 %s 電話 %s", name, idNo, mobile)
+                append(text)
+                val nameIndex = text.indexOf(name)
+                val idNoIndex = text.indexOf(idNo)
+                val mobileIndex = text.indexOf(mobile)
+                setSpan(UnderlineSpan(), nameIndex, nameIndex + name.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+                setSpan(UnderlineSpan(), idNoIndex, idNoIndex + idNo.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+                setSpan(UnderlineSpan(), mobileIndex, mobileIndex + mobile.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+            }
+            isSingleLine = true
+            layout(0, 0, pageHeight, pageWidth)
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize.toFloat())
+            setTextColor(Color.BLACK)
+            text = titleSpannedString
+            isDrawingCacheEnabled = true
+        }.run {
+            page.canvas.drawBitmap(
+                drawingCache,
+                top,
+                textSize.toFloat(),
+                null
+            )
+        }
 
+        // Adding body
+        TextView(context).apply {
+            val bodySpannedString = SpannableStringBuilder().apply {
+                val text = String.format("出生年月日 %s 地址 %s", birth, address)
+                append(text)
+                val birthIndex = text.indexOf(birth)
+                val addressIndex = text.indexOf(address)
+                setSpan(UnderlineSpan(), birthIndex, birthIndex + birth.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+                setSpan(UnderlineSpan(), addressIndex, addressIndex + address.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+            }
+            isSingleLine = true
+            layout(0, 0, pageHeight, pageWidth)
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize.toFloat())
+            setTextColor(Color.BLACK)
+            text = bodySpannedString
+            isDrawingCacheEnabled = true
+        }.run {
+            page.canvas.drawBitmap(
+                drawingCache,
+                top,
+                (textSize * 3).toFloat(),
+                null
+            )
+        }
 
-        //TODO: Add a second textview and paint and everything
-
-        //Adding title
-        val textViewTitle = TextView(context)
-        textViewTitle.layout(0, 0, pageWidth, pageHeight) //text box size heightpx x widthpx
-        textViewTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, headerTextSize.toFloat())
-        textViewTitle.setTextColor(Color.BLACK)
-        //textView.setShadowLayer(5, 2, 2, Color.CYAN); //text shadow
-        textViewTitle.text = "$headingText\n_________________"
-        textViewTitle.isDrawingCacheEnabled = true
-        page.canvas.drawBitmap(textViewTitle.drawingCache, 1f, 1f, null)
-
-        //Adding body
-        val textView = TextView(context)
-        textView.layout(0, 0, pageWidth, pageHeight) //text box size heightpx x widthpx
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, bodyTextSize.toFloat())
-        textView.setTextColor(Color.BLACK)
-        //textView.setShadowLayer(5, 2, 2, Color.CYAN); //text shadow
-        textView.text = bodyText
-        textView.isDrawingCacheEnabled = true
+        val barcodeWidth = pageHeight / 4 * 3
         page.canvas.drawBitmap(
-            textView.drawingCache,
-            1f,
-            (headerTextSize + headerTextSize / 2).toFloat(),
+            createBarcodeBitmap(
+                barcodeValue = idNo,
+                widthPixels = barcodeWidth,
+                heightPixels = textSize * 2
+            ),
+            top - (top / 2) + pageWidth / 2 - barcodeWidth / 2,
+            (textSize * 6).toFloat(),
             null
         )
-        //text box top left position 50,50
 
-//        canvas.save();
-//        canvas.translate(50, 20); //position text on the canvas
+        val paint = Paint()
+        paint.typeface = Typeface.DEFAULT
+        paint.textSize = 48f
+        val textWidth = paint.measureText(idNo, 0, idNo.length)
+
+        TextView(context).apply {
+            isSingleLine = true
+            layout(0, 0, textWidth.toInt(), pageWidth)
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, paint.textSize)
+            setTextColor(Color.BLACK)
+            text = idNo
+            isDrawingCacheEnabled = true
+        }.run {
+            page.canvas.drawBitmap(
+                drawingCache,
+                top - (top / 2) + pageWidth / 2 - textWidth / 2,
+                (textSize * 8).toFloat(),
+                null
+            )
+        }
 
         // finish the page
         document.finishPage(page)
-        var filepath: String? = null
-        try {
-            val mypath = File(context.getExternalFilesDir(null), filename)
-            filepath = mypath.absolutePath
-            document.writeTo(FileOutputStream(mypath))
-        } catch (e: FileNotFoundException) {
-            Log.d("ez_UserAreaActivity", "File not found exception")
-        } catch (e: IOException) {
-            Log.d("ez_UserAreaActivity", "IOException")
-        }
+
+        val filePath = File(context.getExternalFilesDir(null), "test.pdf")
+        document.writeTo(FileOutputStream(filePath.absolutePath))
 
         // close the document
         document.close()
-        return filepath
+        return filePath.absolutePath
+    }
+
+    private fun createBarcodeBitmap(
+        barcodeValue: String,
+        widthPixels: Int,
+        heightPixels: Int
+    ): Bitmap {
+        val bitMatrix = Code128Writer().encode(
+            barcodeValue,
+            BarcodeFormat.CODE_128,
+            widthPixels,
+            heightPixels
+        )
+
+        val pixels = IntArray(bitMatrix.width * bitMatrix.height)
+        for (y in 0 until bitMatrix.height) {
+            val offset = y * bitMatrix.width
+            for (x in 0 until bitMatrix.width) {
+                pixels[offset + x] =
+                    if (bitMatrix.get(x, y)) Color.BLACK else Color.WHITE
+            }
+        }
+
+        val bitmap = Bitmap.createBitmap(
+            bitMatrix.width,
+            bitMatrix.height,
+            Bitmap.Config.ARGB_8888
+        )
+        bitmap.setPixels(
+            pixels,
+            0,
+            bitMatrix.width,
+            0,
+            0,
+            bitMatrix.width,
+            bitMatrix.height
+        )
+        return bitmap
     }
 }
